48b722251c428d98da2b2de5cb8e5e98f91a4f73
Wujutsu <kevin.duballet@gmail.com>
2023-08-21 14:52:15 +0200

    save

--- src/pages/home/productsExemple/ProductsExemple.jsx
+++ src/pages/home/productsExemple/ProductsExemple.jsx
@@ -5,11 +5,13 @@ const ProductsExemple = () => {
   return (
     <section className="list-products">
       <div className="row">
-        {["aze", "e", "ee", "ze"].map((item, index) => (
+        {["Sac", "Chouchou", "Doudou", "ze"].map((item, index) => (
           <div key={index} className="col-md-3 col-sm-6">
             <div className="product-box">
               <div className="picture">
-                <div className="text">Categorie</div>
+                <div className="box-text">
+                  <div className="text">{item}</div>
+                </div>
               </div>
             </div>
           </div>
